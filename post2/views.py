c8fd3d1849f40b265d9d1acc727bca270fcbfe4c
godgi <god871@nate.com>
2020-06-09 19:19:48 +0900

    6.9 과제

--- post2/views.py
+++ post2/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+def first(request):
+    return render(request, 'post2/first.html')
+
+
+def second(request):
+    return render(request, 'post2/second.html')
+
+
+def third(request):
+    return render(request, 'post2/third.html')
